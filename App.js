d193dd5f6300cb52a853a1e4cf588ab036cd2c0f
Elgur <leroy.maxence@yahoo.com>
2020-02-24 14:01:58 +0100

    project complete

---
+++ App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { StyleSheet, View, FlatList, Button } from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState([false]);
+
+  const addGoalHandler = goalTitle => {
+    if (goalTitle.length === 0) {
+      return;
+    }
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalAddition = () => {
+    setIsAddMode(false);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Button title="Add new goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancelGoal={cancelGoalAddition}
+      />
+      {/* FlatList is like a .map but prevent from rendering all items (needs parameters and data being an array of objects with 2 keys, 'key' and the value). If in the oblect there is an Id for exmaple, we have to had another paramater to the FlatList, keyExtractor.  FlastLlist has better performances for a list that can be very long */}
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }
+});
---
+++ components/GoalInput.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet, Button, Modal } from "react-native";
+
+const GoalInput = props => {
+  const [enteredGoal, setEnteredGoal] = useState("");
+
+  const goalInputHandler = enteredGoal => {
+    setEnteredGoal(enteredGoal);
+  };
+
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoal);
+    setEnteredGoal("");
+  };
+
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="CANCEL" color="red" onPress={props.onCancelGoal} />
+          </View>
+          {/* <Button title="ADD " onPress={() => props.onAddGoal(enteredGoal)} /> or */}
+          <View style={styles.button}>
+            <Button title="ADD" onPress={addGoalHandler} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "60%"
+  },
+  button: {
+    width: "40%"
+  }
+});
+
+export default GoalInput;
